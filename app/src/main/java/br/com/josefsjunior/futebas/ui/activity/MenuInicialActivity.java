6eb0b2ee27548cd19f5dd346fa97ce91986f5067
josefsjunior <josefsjunior@outlook.com>
2018-07-16 21:07:12 -0300

    activity de mapa

--- app/src/main/java/br/com/josefsjunior/futebas/ui/activity/MenuInicialActivity.java
+++ app/src/main/java/br/com/josefsjunior/futebas/ui/activity/MenuInicialActivity.java
@@ -1,5 +1,6 @@
 package br.com.josefsjunior.futebas.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,8 @@ public class MenuInicialActivity extends AppCompatActivity {
         botaoProcurarQuadras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MenuInicialActivity.this, ProcurarQuadrasActivity.class);
+                startActivity(intent);
             }
         });
     }
